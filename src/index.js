bc19099b997469dc29873993456634224247f15f
Bhavik Sheth <bhaviksheth20@gmail.com>
2019-03-08 19:08:40 +0000

    install sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import './styles.css';
+import './styles.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from "./configureStore"
