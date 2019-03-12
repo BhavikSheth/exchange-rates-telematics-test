e20fbad4048836aa7951ad3f3479cde331ef8303
Bhavik Sheth <bhaviksheth20@gmail.com>
2019-03-12 11:06:11 +0000

    add styling and content to home and about page

--- src/containers/Home/component.js
+++ src/containers/Home/component.js
@@ -1,9 +1,70 @@
-import React from "react"
+import React from "react";
+
+import styles from "./styles.module.scss";
 
 const Home = props => {
   return (
-    <h1>Home</h1>
-  )
-}
+    <div>
+      <h1 className={styles.header}>Home</h1>
+      <p className={styles.paragraph}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet
+        velit enim, quis volutpat justo tristique a. Vivamus fermentum
+        ullamcorper consectetur. Donec aliquet urna a consectetur varius.
+        Aliquam at arcu in mi tristique pharetra. Nullam augue nisl, gravida
+        aliquet metus eu, sodales facilisis neque. Duis nunc urna, ultrices vel
+        nunc in, porta tempus lectus. Donec urna purus, bibendum vitae placerat
+        quis, lobortis id urna. Morbi varius arcu sed mauris finibus, eget
+        tristique ligula porttitor. Curabitur orci dolor, interdum a lorem non,
+        pellentesque rutrum odio. Mauris ac egestas neque, vitae cursus elit.
+        Fusce elit leo, cursus eu felis a, finibus feugiat libero. Morbi tempus
+        placerat consectetur. Suspendisse potenti. Nullam commodo pretium
+        fringilla. Fusce in fermentum neque. Cras lobortis magna dolor, vel
+        facilisis mauris tempus imperdiet. Vivamus dignissim vitae lectus at
+        hendrerit. Morbi id ex auctor, finibus dui a, vestibulum lorem. Integer
+        sit amet ex non felis eleifend eleifend. Nullam id lacinia felis, in
+        luctus libero. Class aptent taciti sociosqu ad litora torquent per
+        conubia nostra, per inceptos himenaeos. Nam condimentum est id leo
+        porta, nec mollis risus ultricies. Aliquam massa orci, cursus mollis
+        commodo nec, egestas id sapien. Proin porttitor massa vel odio commodo,
+        at elementum sem placerat. Donec gravida libero at diam blandit, at
+        tempor enim consequat. Nam facilisis placerat tincidunt. Cras quis
+        tortor euismod, porta tortor a, tincidunt ante. Suspendisse potenti. Ut
+        imperdiet volutpat elit et rhoncus. Morbi leo lorem, pellentesque ut
+        porta quis, fermentum ut metus. Nulla id hendrerit nisi. Praesent sed
+        nisl ut dolor vestibulum porta et ac massa. Donec efficitur pharetra
+        urna, a elementum urna commodo id. Integer ornare elit semper eros
+        tincidunt, sit amet bibendum neque aliquet. Ut a velit ullamcorper,
+        sagittis magna ac, tincidunt diam. Vestibulum sit amet mattis ligula.
+        Donec quis venenatis velit. Nunc molestie elit purus, non tristique
+        metus rhoncus non. Aliquam erat volutpat. Mauris vel malesuada mi, eget
+        tempus mi. Donec vitae efficitur lacus. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos. Sed turpis
+        ligula, ultrices quis ligula in, semper semper tortor. Suspendisse
+        potenti. Cras mauris urna, blandit in pulvinar ac, ornare sit amet
+        tellus. Pellentesque consequat nulla risus, non commodo neque malesuada
+        eu. Suspendisse cursus dictum magna ut finibus. Phasellus pulvinar
+        tellus laoreet nisl lacinia commodo. Curabitur lacinia turpis id tellus
+        fermentum auctor. Aenean condimentum augue purus. Suspendisse
+        sollicitudin mattis augue, eu porta sem maximus quis. Etiam pellentesque
+        eleifend turpis, ut accumsan arcu. Vestibulum placerat vestibulum purus
+        ac rhoncus. Morbi porttitor fringilla erat, at sagittis velit accumsan
+        eget. Aenean ullamcorper elit libero, ac luctus neque efficitur a.
+        Curabitur urna libero, ullamcorper eu ex eget, eleifend accumsan arcu.
+        In in velit vehicula, accumsan sem vitae, euismod nunc. Morbi tortor
+        lacus, bibendum et auctor a, semper eget orci. Integer mollis sapien eu
+        nisi finibus, in elementum lorem euismod. Phasellus facilisis accumsan
+        purus, vel sodales mauris maximus a. Fusce purus ligula, semper quis
+        dolor id, bibendum commodo orci. Aenean nec arcu tempor, sollicitudin
+        mauris eu, imperdiet libero. Nulla ullamcorper tincidunt neque, in
+        ultrices felis tristique ac. Pellentesque nec luctus nisi. Donec metus
+        eros, consequat et aliquet vitae, tempor vitae sem. Duis dictum
+        efficitur elit, id ultricies augue lacinia condimentum. Aliquam vehicula
+        sed sem sed gravida. Aenean rutrum efficitur tellus sed dictum. Quisque
+        ac finibus ex. Morbi porta ac ex sollicitudin lobortis. Interdum et
+        malesuada fames ac ante ipsum primis in faucibus.
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
